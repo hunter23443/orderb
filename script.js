6671475939e5cac37b4cd4a7c8f5ab62fd7880c4
hunter23443 <abdullahali.sec@student.uobabylon.edu.iq>
2024-04-09 13:13:51 +0300

    ل

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-// let imgs = document.querySelectorAll('.img-select a');
-// let imgBtns = [...imgs];
-// let imgId = 1;
+let imgs = document.querySelectorAll('.img-select a');
+let imgBtns = [...imgs];
+let imgId = 1;
 
-// imgBtns.forEach((imgItem) => {
-//     imgItem.addEventListener('click' , (event) => {
-//         event.preventDefault();
-//         imgId = im..............
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click' , (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
 
+function slideImage() {
+    let displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * -displayWidth}px)`;
+}
 
+window.addEventListener('resize' , slideImage);
